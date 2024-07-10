8116c2264481684741d26f110b2d790ad927f29c
vinit1511 <vinit1511raj@gmail.com>
2024-07-10 22:19:56 +0530

    Change NLP to LLM

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'auth/sign-in', pathMatch: 'full' },
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: 'attempt-question', loadChildren: () => import('./attempt-question/attempt-question.module').then(m => m.AttemptQuestionModule) },
   { path: 'test-board/:id', loadChildren: () => import('./test-board/test-board.module').then(m => m.TestBoardModule) },
